b5002e2ccbdab670360316b73c3c8c5be4ef068d
John Hoerr <jhoerr@gmail.com>
2018-10-19 16:29:29 -0400

    Remove boilerplate comments

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -13,7 +13,6 @@ interface ILocationProps {
 interface ISigninProps {
     location: ILocationProps
 }
-// We can use `typeof` here to map our dispatch types to the props, like so.
 interface IPropsFromDispatch {
     postSignInRequest: typeof Auth.postSignInRequest
 }
@@ -30,11 +29,6 @@ class Signin extends React.Component<Auth.IState & ISigninProps & IPropsFromDisp
             <>
                 { this.props.loading &&
                   <p>Signing in...</p> }
-                { this.props.data &&
-                  <div>
-                    <p>Username: {this.props.data.user_name} </p>
-                    <p>Role: {this.props.data.user_role} </p>
-                  </div> }
                 { !this.props.loading && this.props.error &&
                   <div>
                     <p>Errors: {this.props.error}</p> 
@@ -44,20 +38,13 @@ class Signin extends React.Component<Auth.IState & ISigninProps & IPropsFromDisp
     };
 }
 
-// It's usually good practice to only include one context at a time in a connected component.
-// Although if necessary, you can always include multiple contexts. Just make sure to
-// separate them from each other to prevent prop conflicts.
 const mapStateToProps = (state: IApplicationState) => 
     state.auth
   
-  // mapDispatchToProps is especially useful for constraining our actions to the connected component.
-  // You can access these via `this.props`.
-  const mapDispatchToProps = (dispatch: Dispatch) => ({
+const mapDispatchToProps = (dispatch: Dispatch) => ({
     postSignInRequest: (request:Auth.IAuthRequest) => dispatch(Auth.postSignInRequest(request))
-  })
+})
   
-// Now let's connect our component!
-// With redux v4's improved typings, we can finally omit generics here.
 export default connect<Auth.IState, IPropsFromDispatch>(
     mapStateToProps,
     mapDispatchToProps
