4aa9ba7057cd23d6553aa648b55dd8f6f09e6f04
John Hoerr <jhoerr@gmail.com>
2018-11-12 12:26:28 -0500

    Add TeamCity scripts

---
+++ .teamcity/docker-service-update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Export the tag name
+export DOCKER_TAG=${TEAMCITY_BRANCH/refs\/heads\//}
+
+# Source the Docker client bundle for the environment associated with this build.
+source $HOME/.dcd/$DOCKER_UCP_BUNDLE.sh
+
+# Update the service and non-secret environment variables
+echo Updating $DOCKER_SERVICE service from $DOCKER_HUB_REPO:$DOCKER_TAG
+docker service update --image $DOCKER_HUB_REPO:$DOCKER_TAG \
+    --health-cmd 'curl --fail localhost:80 || exit 1' \
+    --health-interval 2s \
+    --health-retries 120 \
+    --health-start-period 10s \
+    --health-timeout 5s \
+    $DOCKER_SERVICE
