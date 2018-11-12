3e4453fb35cd928e2f5e424d8e5a9b5a9cf6c18c
John Hoerr <jhoerr@gmail.com>
2018-11-12 14:06:24 -0500

    Debuggin TeamCity

--- .teamcity/docker-build-tag-push.sh
+++ .teamcity/docker-build-tag-push.sh
@@ -6,8 +6,8 @@ export DOCKER_TAG=${TEAMCITY_BRANCH/refs\/heads\//}
 # Configure the environment variables. 
 # Adding a '.local' gives these variables precedence.
 # See: https://medium.com/@tacomanator/environments-with-create-react-app-7b645312c09d
-echo Copy .env.$ENVIRONMENT -> .env.$ENVIRONMENT.local
-cp .env.$ENVIRONMENT .env.$ENVIRONMENT.local
+#echo Copy .env.$ENVIRONMENT -> .env.$ENVIRONMENT.local
+#cp .env.$ENVIRONMENT .env.$ENVIRONMENT.local
 
 # Build the docker image and tag it for docker hub
 docker build -t app .
