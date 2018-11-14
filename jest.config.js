f0ff8b70b68622d216fa3de4addd03ae8e71f29d
Andy Orahood <jorahood@iu.edu>
2018-11-14 14:50:00 -0600

    docs: commented why I set testURL to a random url

--- jest.config.js
+++ jest.config.js
@@ -17,5 +17,7 @@ module.exports = {
   "watchPathIgnorePatterns": [
     "contracts/"
   ],
+  // setting an actual url for jsdom rather than default of "about:config"
+  // to avoid SecurityError
   "testURL": "https://localhost:3000"
 }
