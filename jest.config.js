4a0bb6b9f1b65606a1872b01e4b6328881e970ed
Andy Orahood <jorahood@iu.edu>
2018-11-14 14:25:28 -0600

    fix: allows committing pact file while running test in watch mode
    I noticed a problem with writing the pact file to the project rather
    than a build dir- running tests in watch mode would loop because each
    test run would change the pact file, triggering tests to run again. In
    order to configure jest to not watch the contracts directory, I had to
    add jest.config.js to exclude it as create-react-app doesn't allow that
    and modify the "test" task to use jest rather than the react-scripts
    version

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "watchPathIgnorePatterns": [
+    "contracts/"
+  ],
+  "testURL": "https://localhost:3000"
+}
