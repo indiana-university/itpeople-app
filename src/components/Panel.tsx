919599e3cdfcd514071c2a51d6a6b13e9494d2b2
Kenneth Jones <kendjone@iu.edu>
2018-11-30 11:28:05 -0600

    Update panel style

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -2,8 +2,11 @@ import * as React from "react";
 import { Col, Row } from "rivet-react";
 
 export const Panel: React.SFC<IProps> = ({ title, children }) => (
-  <Row className="rvt-border-all rvt-border-radius rvt-bg-white">
-    <Col>
+  <Row>
+    <Col
+      className="rvt-border-all rvt-border-radius rvt-bg-white"
+      style={{ overflow: "auto" }}
+    >
       <Row>
         <Col
           className="rvt-bg-cream rvt-border-bottom rvt-p-all-xs rvt-p-left-md rvt-p-right-md rvt-ts-18 rvt-text-bold"
