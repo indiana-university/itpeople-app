690f203331f6624a56954d1e90db1b14ed10a59d
Andy Orahood <jorahood@indiana.edu>
2018-12-08 05:12:47 -0600

    sets up env & fetch in jest jsdom

--- jest.config.js
+++ jest.config.js
@@ -19,5 +19,6 @@ module.exports = {
   ],
   // setting an actual url for jsdom rather than default of "about:config"
   // to avoid SecurityError
-  "testURL": "https://localhost:3000"
+  "testURL": "https://localhost:3000",
+  "setupFiles": ["./jestSetEnv.js"]
 }
---
+++ jestSetEnv.js
@@ -0,0 +1,1 @@
+process.env = Object.assign(process.env, { REACT_APP_API_URL: 'http://localhost:3001' })
