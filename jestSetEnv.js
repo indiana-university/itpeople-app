935241f52e455b541e5c8a1cd84c277c9f71f07b
Andy Orahood <jorahood@indiana.edu>
2018-12-10 14:38:32 -0600

    digs down into effects to get payload

--- jestSetEnv.js
+++ jestSetEnv.js
@@ -1,1 +1,3 @@
-process.env = Object.assign(process.env, { REACT_APP_API_URL: 'http://localhost:3001' })
+process.env = Object.assign(process.env, {
+  REACT_APP_API_URL: 'http://localhost:3001'
+})
--- src/integration.test.ts
+++ src/integration.test.ts
@@ -5,7 +5,7 @@ import { IApiState, IApplicationState, IEntity } from "./components/types";
 import { Reducer } from 'redux';
 import 'whatwg-fetch'
 
-it('just works!', () => {
+it('fetches', () => {
   const reducer: Reducer = (state, action) => {
     return {
       profile: {
@@ -15,9 +15,15 @@ it('just works!', () => {
   }
   return expectSaga(profileSaga)
     .withReducer(reducer)
+    .dispatch({ type: ProfileActionTypes.PROFILE_FETCH_REQUEST, payload: { id: 1 } })
     .put.actionType(
       ProfileActionTypes.PROFILE_FETCH_SUCCESS
     )
-    .dispatch({ type: ProfileActionTypes.PROFILE_FETCH_REQUEST, payload: { id: 1 } })
     .run()
+    .then((result) => {
+      const { allEffects } = result;
+
+      expect(allEffects[allEffects.length - 1]["PUT"]["action"]["payload"]).toMatchObject({"name": "Ron Swanson"})
+      
+    })
 })
