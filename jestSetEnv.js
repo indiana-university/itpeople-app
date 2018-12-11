5eebf7a28f3accc6f668fd756e1b56ed1fccb5a8
Andy Orahood <jorahood@indiana.edu>
2018-12-11 13:06:53 -0600

    sets up jest node env for sagas

--- jestSetEnv.js
+++ jestSetEnv.js
@@ -1,3 +1,8 @@
+fetch = require('node-fetch')
+
+var Storage = require('dom-storage')
+localStorage = new Storage('./localStorage.json', { strict: false, ws: '  ' })
+
 process.env = Object.assign(process.env, {
   REACT_APP_API_URL: 'http://localhost:3001'
 })
