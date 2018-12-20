0713ca34fb520f8f778bca595611cec7a7dea65d
Andy Orahood <jorahood@indiana.edu>
2018-12-20 10:56:51 -0600

    adds verifier

---
+++ src/contractVerification.pact.test.ts
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment node
+ */
+
+/**
+* Copyright (C) 2018 The Trustees of Indiana University
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+import * as path from 'path'
+import { Pact, Matchers } from '@pact-foundation/pact'
+import axios from 'axios'
