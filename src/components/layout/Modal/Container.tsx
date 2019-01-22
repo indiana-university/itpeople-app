2888f45ea5f8b5507c3dabba62176c7fbc533989
Kenneth Jones <github@kennethdjones.com>
2019-01-22 09:47:48 -0600

    Add button props to modal

--- src/components/layout/Modal/Container.tsx
+++ src/components/layout/Modal/Container.tsx
@@ -1,4 +1,4 @@
-/** 
+/**
  * Copyright (C) 2018 The Trustees of Indiana University
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -10,50 +10,57 @@ import { IApplicationState } from "../../types";
 import { closeModal, openModal } from "./store";
 import { Modal as RivetModal, Button } from "rivet-react";
 import { ModalProps } from "rivet-react/build/dist/components/Modal/Modal";
+import { ButtonProps } from "rivet-react/build/dist/components/Button/Button";
 
 const Container: React.SFC<IElementProps> = (props: IProps) => {
-    const id = props.id;
-    let button = <Button type="button" onClick={() => props.openModal(id)}>
-        {props.buttonText}
+  const id = props.id;
+  const { variant, size, modifier, innerRef } = props;
+  const buttonProps = { variant, size, modifier, innerRef } ;
+  let button = (
+    <Button type="button" {...buttonProps} onClick={() => props.openModal(id)}>
+      {props.buttonText}
     </Button>
-    const isOpen = id == props.current;
-    if (isOpen && props.onOpen) {
-        props.onOpen()
-    }
-    return <>
-        {button}
-        <RivetModal {...props} isOpen={isOpen} onDismiss={props.closeModal} />
+  );
+  const isOpen = id == props.current;
+  if (isOpen && props.onOpen) {
+    props.onOpen();
+  }
+  return (
+    <>
+      {button}
+      <RivetModal {...props} isOpen={isOpen} onDismiss={props.closeModal} />
     </>
-}
+  );
+};
 
 const mapDispatchToProps = (dispatch: Dispatch): IDispatchProps => ({
-    closeModal: () => dispatch(closeModal()),
-    openModal: (id) => dispatch(openModal(id))
+  closeModal: () => dispatch(closeModal()),
+  openModal: id => dispatch(openModal(id))
 });
 
 interface IDispatchProps {
-    closeModal: typeof closeModal;
-    openModal: typeof openModal;
+  closeModal: typeof closeModal;
+  openModal: typeof openModal;
 }
 interface IStateProps {
-    current: string,
+  current: string;
 }
 
-interface IElementProps extends ModalProps {
-    id: string,
-    children?: any,
-    title: string,
-    buttonText?: string | typeof Button,
-    onOpen?: () => any
+interface IElementProps extends ModalProps, ButtonProps {
+  id: string;
+  children?: any;
+  title: string;
+  buttonText?: string | typeof Button;
+  onOpen?: () => any;
 }
 
-interface IProps extends IDispatchProps, IStateProps, IElementProps { }
+interface IProps extends IDispatchProps, IStateProps, IElementProps {}
 
 export const Modal = connect(
-    (state: IApplicationState) => {
-        return {
-            current: state.modal
-        }
-    },
-    mapDispatchToProps
+  (state: IApplicationState) => {
+    return {
+      current: state.modal
+    };
+  },
+  mapDispatchToProps
 )(Container);
