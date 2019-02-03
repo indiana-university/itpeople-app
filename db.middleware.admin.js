1bee0d30c07e0d22e4635044438cf33a433efb8e
Kenneth Jones <github@kennethdjones.com>
2019-02-03 11:47:42 -0600

    Check if authorization header exist to emulate a 401 Error

--- db.middleware.admin.js
+++ db.middleware.admin.js
@@ -1,5 +1,8 @@
 module.exports = (req, res, next) => {
   // Admin with read/write/create/delete permission to every resource
+  if(!res.header("Authorization")){
+    res.sendStatus(401);
+  }
   res.header('Access-Control-Expose-Headers', 'X-User-Permissions');
   res.header('X-User-Permissions', 'GET, PUT, POST, DELETE');
   next()
