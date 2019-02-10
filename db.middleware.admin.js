62ff0ec1e988ec32ca74c2a8b59fa94b50ad71af
Kenneth Jones <github@kennethdjones.com>
2019-02-10 14:06:54 -0600

    Allow /auth to be requested without authorization header

--- db.middleware.admin.js
+++ db.middleware.admin.js
@@ -1,10 +1,10 @@
 module.exports = (req, res, next) => {
   // Admin with read/write/create/delete permission to every resource
-  if (!req.get("Authorization")) {
-    res.sendStatus(401);
+  if (!req.get("Authorization") && req.path != "/auth") {
+    res.sendStatus(401)
   } else {
     res.header("Access-Control-Expose-Headers", "X-User-Permissions");
     res.header("X-User-Permissions", "GET, PUT, POST, DELETE");
-    next();
   }
+  next();
 };
