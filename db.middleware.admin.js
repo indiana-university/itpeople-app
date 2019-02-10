c3ab0914c237a5653368227cab4789ab186929dd
Kenneth Jones <github@kennethdjones.com>
2019-02-10 12:56:05 -0600

    Get authorization on the request; Do not set authorization as the response header

--- db.middleware.admin.js
+++ db.middleware.admin.js
@@ -1,9 +1,10 @@
 module.exports = (req, res, next) => {
   // Admin with read/write/create/delete permission to every resource
-  if(!res.header("Authorization")){
+  if (!req.get("Authorization")) {
     res.sendStatus(401);
+  } else {
+    res.header("Access-Control-Expose-Headers", "X-User-Permissions");
+    res.header("X-User-Permissions", "GET, PUT, POST, DELETE");
+    next();
   }
-  res.header('Access-Control-Expose-Headers', 'X-User-Permissions');
-  res.header('X-User-Permissions', 'GET, PUT, POST, DELETE');
-  next()
-}
+};
