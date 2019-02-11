e09d08b148e42c061a6697649a3f08e9558f2268
Kenneth Jones <github@kennethdjones.com>
2019-02-11 08:45:35 -0600

    Update search reducer to store search results

--- src/components/Search/store.ts
+++ src/components/Search/store.ts
@@ -1,37 +1,30 @@
-/** 
+/**
  * Copyright (C) 2018 The Trustees of Indiana University
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 import { action } from "typesafe-actions";
-import {
-    IEntity,
-    IApiState,
-    TaskErrorReducer,
-    TaskStartReducer,
-    TaskSuccessReducer,
-    defaultState
-} from "../types";
+import { IEntity, IApiState, TaskErrorReducer, TaskStartReducer, defaultState } from "../types";
 import { SearchLists } from "./Results";
 
 //#region TYPES
 export const enum SearchActionTypes {
-    SEARCH_SIMPLE_SUBMIT = "@@search/SEARCH_SIMPLE_SUBMIT",
-    SEARCH_SIMPLE_FETCH_REQUEST = "@@search/SEARCH_SIMPLE_FETCH_REQUEST",
-    SEARCH_SIMPLE_FETCH_SUCCESS = "@@search/SEARCH_SIMPLE_FETCH_SUCCESS",
-    SEARCH_SIMPLE_FETCH_ERROR = "@@search/SEARCH_SIMPLE_FETCH_ERROR",
-    SEARCH_SET_CURRENT_LIST = "SEARCH_SET_CURRENT_LIST"
+  SEARCH_SIMPLE_SUBMIT = "@@search/SEARCH_SIMPLE_SUBMIT",
+  SEARCH_SIMPLE_FETCH_REQUEST = "@@search/SEARCH_SIMPLE_FETCH_REQUEST",
+  SEARCH_SIMPLE_FETCH_SUCCESS = "@@search/SEARCH_SIMPLE_FETCH_SUCCESS",
+  SEARCH_SIMPLE_FETCH_ERROR = "@@search/SEARCH_SIMPLE_FETCH_ERROR",
+  SEARCH_SET_CURRENT_LIST = "SEARCH_SET_CURRENT_LIST"
 }
 
 export interface ISimpleSearchRequest {
-    term: string;
+  term: string;
 }
 
 export interface ISimpleSearchResult extends ISimpleSearchRequest {
-    departments: IEntity[];
-    units: IEntity[];
-    users: IEntity[];
-    selectedList?: SearchLists;
+  departments: IEntity[];
+  units: IEntity[];
+  users: IEntity[];
+  selectedList?: SearchLists;
 }
 //#endregion
 
@@ -43,65 +36,64 @@ export const fetchSuccess = (resp: IApiResponse<ISimpleSearchResult>) => action(
 export const fetchError = (error: string) => action(SearchActionTypes.SEARCH_SIMPLE_FETCH_ERROR, error);
 //#endregion
 
-
 import { Reducer, AnyAction } from "redux";
 
-export interface IState
-    extends IApiState<ISimpleSearchRequest, ISimpleSearchResult> { }
+export interface IState extends IApiState<ISimpleSearchRequest, ISimpleSearchResult> {}
 
-export const initialState: IState = defaultState()
+export const initialState: IState = defaultState();
 
 export const reducer: Reducer<IState> = (state = initialState, act) => {
-    switch (act.type) {
-        case SearchActionTypes.SEARCH_SIMPLE_FETCH_REQUEST:
-            return TaskStartReducer(state, act);
-        case SearchActionTypes.SEARCH_SIMPLE_FETCH_SUCCESS:
-            if (act.payload) {
-                if (act.payload.users && act.payload.users.length) {
-                    act.payload.selectedList = SearchLists.People;
-                } else if (act.payload.units && act.payload.units.length) {
-                    act.payload.selectedList = SearchLists.Units;
-                } else if (act.payload.departments && act.payload.departments.length) {
-                    act.payload.selectedList = SearchLists.Departments;
-                }
-            }
-            return TaskSuccessReducer(state, act);
-        case SearchActionTypes.SEARCH_SIMPLE_FETCH_ERROR:
-            return TaskErrorReducer(state, act);
-        case SearchActionTypes.SEARCH_SET_CURRENT_LIST:
-            if (state.data) {
-                return {
-                    ...state,
-                    data: { ...state.data, selectedList: act.payload.list },
-                    error: undefined,
-                    loading: false,
-                    request: undefined
-                };
-            }
-        default:
-            return state;
-    }
+  switch (act.type) {
+    case SearchActionTypes.SEARCH_SIMPLE_FETCH_REQUEST:
+      return TaskStartReducer(state, act);
+    case SearchActionTypes.SEARCH_SIMPLE_FETCH_SUCCESS:
+      if (act.payload) {
+        if (act.payload.users && act.payload.users.length) {
+          act.payload.selectedList = SearchLists.People;
+        } else if (act.payload.units && act.payload.units.length) {
+          act.payload.selectedList = SearchLists.Units;
+        } else if (act.payload.departments && act.payload.departments.length) {
+          act.payload.selectedList = SearchLists.Departments;
+        }
+      }
+      // TODO: should payload be an IApiResponse?
+      return { ...state, data: act.payload, loading: false };
+    case SearchActionTypes.SEARCH_SIMPLE_FETCH_ERROR:
+      return TaskErrorReducer(state, act);
+    case SearchActionTypes.SEARCH_SET_CURRENT_LIST:
+      if (state.data) {
+        return {
+          ...state,
+          data: { ...state.data, selectedList: act.payload.list },
+          error: undefined,
+          loading: false,
+          request: undefined
+        };
+      }
+    default:
+      return state;
+  }
 };
 
-import { all, fork, put, takeEvery, } from 'redux-saga/effects';
-import { restApi, IApi, IApiResponse } from 'src/components/api';
+import { all, fork, put, takeEvery } from "redux-saga/effects";
+import { restApi, IApi, IApiResponse } from "src/components/api";
 import { signinIfUnauthorized } from "../effects";
 
 const api = restApi();
 
 function* handleFetch(api: IApi, req: ISimpleSearchRequest) {
-    const action = yield api
-        .get<ISimpleSearchResult>(`/search?term=${req.term}`)
-        .then(fetchSuccess)
-        .catch(signinIfUnauthorized)
-        .catch(fetchError)
-    yield put(action)
+  const action = yield api
+    .get<ISimpleSearchResult>(`/search?term=${req.term}`)
+    .then(fetchSuccess)
+    .catch(signinIfUnauthorized)
+    .catch(fetchError);
+  yield put(action);
 }
 
 function* watchSimpleSearchFetch() {
-    yield takeEvery(SearchActionTypes.SEARCH_SIMPLE_FETCH_REQUEST, (a:AnyAction) => handleFetch(api, a.payload))
+  yield takeEvery(SearchActionTypes.SEARCH_SIMPLE_FETCH_REQUEST, (a: AnyAction) => handleFetch(api, a.payload));
 }
 
 export function* saga() {
-    yield all([ fork(watchSimpleSearchFetch) ])
+  yield all([fork(watchSimpleSearchFetch)]);
 }
