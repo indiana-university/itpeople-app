34a297301c23adf92ee26bcd3c500037fc8ba9f5
Kenneth Jones <github@kennethdjones.com>
2019-02-13 09:56:03 -0600

    Add more chaos

--- db.middleware.chaos.js
+++ db.middleware.chaos.js
@@ -1,19 +1,31 @@
 module.exports = (req, res, next) => {
   // let's not mess with auth for now
   if (req.path == "/auth") {
+    // login works 50% of the time
+    if (Math.random() > 0.5) {
+      res.sendStatus(401);
+    }
     return next();
   }
 
   // random server errors
   if (Math.random() > 0.9) {
-    res.sendStatus(500);
+    // 10% server errors
+    return res.sendStatus(500);
   }
   if (Math.random() > 0.8) {
-    res.sendStatus(403);
+    // 10% forbidden
+    return res.sendStatus(403);
   }
   if (Math.random() > 0.7) {
-    res.sendStatus(404);
+    //  10% not found
+    return res.sendStatus(404);
   }
 
+  if (Math.random() > 0.3) {
+    // 10% timeout
+    req.setTimeout(10);
+    return setTimeout(next, 100);
+  }
   next();
 };
