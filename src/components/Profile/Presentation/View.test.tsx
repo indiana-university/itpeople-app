20796322e083a463ce324e86a40c3c4fa762fd1e
Andy Orahood <jorahood@indiana.edu>
2019-02-28 10:56:26 -0600

    profile view integration tests

--- src/components/Profile/Presentation/View.test.tsx
+++ src/components/Profile/Presentation/View.test.tsx
@@ -13,7 +13,7 @@ import { render } from 'react-testing-library'
 import { View } from './View'
 import { initialState, toggleUnit } from "../store";
 import configureStore from 'src/configureStore'
-import { defaultState, IPerson } from 'src/components/types'
+import { defaultState, IPerson, IUnitMembership, IUnit } from 'src/components/types'
 import * as examples from 'src/db.json'
 import { GlobalWithFetchMock } from "jest-fetch-mock"
 
@@ -21,7 +21,7 @@ const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock
 customGlobal.fetch = require('jest-fetch-mock')
 customGlobal.fetchMock = customGlobal.fetch
 
-it('shows the name of the person', () => {
+it('displays the name of the person', () => {
     const history = createMemoryHistory({ initialEntries: ['/person/1'] })
     const person1: IPerson = examples.people[0]
     const person1State = { ...defaultState(), data: person1 }
@@ -36,5 +36,43 @@ it('shows the name of the person', () => {
         </Provider>)
 
     expect(getByText(person1.name)).toBeInTheDocument()
+})
+
+it('shows a loading message while loading person', () => {
+    const history = createMemoryHistory({ initialEntries: ['/person/1'] })
+    const person1: IPerson = examples.people[0]
+    const person1State = { ...defaultState(), data: person1, loading: true }
+
+    const { getByLabelText } = render(
+        <Provider store={configureStore(history)}>
+            <ConnectedRouter history={history}>
+                <View {...initialState}
+                    toggleUnit={toggleUnit}
+                    person={person1State} />
+            </ConnectedRouter>
+        </Provider>)
+
+    expect(getByLabelText("Content loading")).toBeInTheDocument()
+})
+
+it('displays unit name for unit memberships for that person', () => {
+    const history = createMemoryHistory({ initialEntries: ['/person/1'] })
+    const person1: IPerson = examples.people[0]
+    const p1Memb1Unit: IUnit = examples.units[0]
+    const p1Memberships: Array<IUnitMembership> = [{ ...examples.memberships[0], unit: p1Memb1Unit }]
+    const person1State = { ...defaultState(), data: person1 }
+    const p1MembershipsState = { ...defaultState(), data: p1Memberships }
+
+    const { getByText } = render(
+        <Provider store={configureStore(history)}>
+            <ConnectedRouter history={history}>
+                <View {...initialState}
+                    toggleUnit={toggleUnit}
+                    person={person1State}
+                    memberships={p1MembershipsState} />
+            </ConnectedRouter>
+        </Provider>)
+
+    expect(getByText(p1Memb1Unit.name)).toBeInTheDocument()
 })
 
