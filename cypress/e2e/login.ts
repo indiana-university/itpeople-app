d71ede9ccb4d51bd12953e5de069c485e7d50e94
Andy Orahood <jorahood@indiana.edu>
2019-04-09 14:17:51 -0500

    login/out e2e test
    -UPDATE YARN TO 1.13 as extra "--" are no longer required for
    passing arguments to nested scripts
    
    -A bug required updating react and react-dom to 16.6
    
    -uses a short-circuit url for oauth that points directly back to the
    url passed to the auth server.
    -uses typescript for test files
    to start the json-server, then the app, then the cypress server I used
    start-server-and-test from npm and refactored the package.json scripts
    -Need to integrate cypress with circleci

--- cypress/e2e/login.ts
+++ cypress/e2e/login.ts
@@ -1,6 +1,11 @@
 
 describe('user login', () => {
-    it('can log in a user', () => {
-        cy.visit('/') 
-    })
+    it('should log in and out', () => {
+        cy.visit('/')
+        cy.getByText(/login/i).click()
+        cy.queryByText(/login/i).should('not.exist')
+        cy.getByText('johndoe').click()
+        cy.getByText(/log out/i).click()
+        cy.queryByText(/login/i).should('exist')
+        })
 })
