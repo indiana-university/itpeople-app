971246ed677c3e32537372300c358783da9d4616
Andy Orahood <jorahood@indiana.edu>
2019-04-22 12:34:38 -0500

    adds type checks for 2 more fixtures

--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -5,7 +5,7 @@ import { createMemoryHistory } from 'history'
 import { render as rtlRender } from 'react-testing-library'
 import configureStore from 'src/configureStore'
 import { GlobalWithFetchMock } from "jest-fetch-mock"
-import { IPerson, IUnit, IDepartment, IUnitMember, ITool, IUnitMemberTool } from 'src/components/types'
+import { IPerson, IUnit, IDepartment, IUnitMember, ITool, IUnitMemberTool, ISupportRelationship } from 'src/components/types'
 import * as examples from 'src/db.json'
 
 // expressive expectation methods for dom queries
@@ -30,11 +30,16 @@ export function render(ui: any, options = { route: '' }) {
     )
 }
 
+// static tests that fixtures conform to types by setting type with 
+// object literal to force Typescript to check all members for exact
+// conformation to type
+
 export const Examples = {
     person: { ...examples.people[0] } as IPerson,
     unit: { ...examples.units[0] } as IUnit,
     member: { ...examples.memberships[0] } as IUnitMember,
     department: { ...examples.departments[0] } as IDepartment,
     tools: { ...examples.tools[0] } as ITool,
-    memberTools: { ...examples.memberTools[0] } as IUnitMemberTool
+    memberTool: { ...examples.memberTools[0] } as IUnitMemberTool, 
+    supportRelationship: {...examples.supportRelationships[0] } as ISupportRelationship
 }
