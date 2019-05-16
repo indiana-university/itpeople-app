4d15c14e3aec6fb56c775764fe58cddd78eac54c
Andy Orahood <jorahood@indiana.edu>
2019-05-16 14:36:03 -0500

    adds snapshot test for <Page>
    mocking the rivet-react shortuid genenerator in the <Footer> to make
    the id identical for each render

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,12 @@
 import * as React from 'react'
 import { render } from 'src/testUtils'
 import App from './App'
+import { create } from 'react-test-renderer'
+import configureStore from 'src/configureStore'
+import { Provider } from 'react-redux'
+import { createMemoryHistory } from 'history'
+import { ConnectedRouter } from 'connected-react-router';
+import { Page } from './components/layout';
 
 it('renders the home page at /', () => {
     const { getByTestId } = render(
@@ -39,4 +45,20 @@ it('renders a profile page at "/me"', () => {
         <App />,
         { route: '/me' })
     expect(getByTestId("profile-page")).toBeInTheDocument()
+})
+
+import * as Rivet from 'rivet-react/build/dist/components/util/Rivet'
+it('renders Page with uits-rivet style', () => {
+    //mock Rivet's shortuid to lock it for testing
+    // @ts-ignore
+    Rivet.shortuid = jest.fn( () => 'id_TESTING')
+    
+    const history = createMemoryHistory({})
+    const rendered = create(
+        <Provider store={configureStore(history)}>
+            <ConnectedRouter history={history}>
+                <Page />
+            </ConnectedRouter>
+        </Provider>)
+    expect(rendered).toMatchSnapshot()
 })
