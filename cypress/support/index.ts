10816a4a8b348aea2b9762776ae90c096caf16fa
Andy Orahood <jorahood@indiana.edu>
2019-06-03 15:20:06 -0500

    stub redirectToLogin to log in for tests, refactor tests for breaking changes in cypress-testing-library.
    There are bugs in cy-test-lib at the moment where getByText() is finding
    multiple elements where it shouldn't so I'm working around it by using
    getAllByText().first()

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,8 +1,15 @@
 import 'cypress-testing-library/add-commands'
 import './commands'
+import * as effects from '../../src/components/effects'
 
 // reset json-server db before each test
 
 beforeEach(() => {
     cy.exec('git checkout src/db.json')
+})
+
+beforeEach(() => {           
+    cy.stub(effects, 'redirectToLogin', () => {
+        window.location.assign('http://localhost:3000/signin?code=secret')
+  })
 })
