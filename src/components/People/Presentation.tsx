9c02b8894d14677d166073ce2931b05a0ace57c9
John Hoerr <jhoerr@gmail.com>
2019-07-29 19:52:37 -0400

    Add position and interest to people list

--- src/components/People/Presentation.tsx
+++ src/components/People/Presentation.tsx
@@ -43,10 +43,14 @@ const Presentation: React.SFC<IProps> = ({ people: { data: people, permissions }
         {people
           .sort(EntityComparer)
           .map((r, i) => (
-            <Col key={"unit:" + i} md={5} className="rvt-p-bottom-lg">
-              <a href={`/units/${r.id}`} className="rvt-link-bold">
+            <Col key={"people:" + i} md={5} className="rvt-p-bottom-lg">
+              <a href={`/people/${r.id}`} className="rvt-link-bold">
                 {r.name}
-              </a> ({r.netId}; {r.department && r.department.name})
+              </a> {r.department && `(${r.department.name})` }
+              <p>
+                {r.position && <><em>Position</em>: {r.position}<br/></>}             
+                {r.expertise && <><em>Interests</em>: {r.expertise.replace(/,/g, ", ")}</>}
+              </p>              
             </Col>
           ))}
       </Row>
