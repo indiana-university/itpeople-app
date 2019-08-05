acc9af29c10a1cfb7244bc4c151c9ef6abacfa57
John Hoerr <jhoerr@gmail.com>
2019-08-05 13:39:54 -0400

    Add subleads to unit member list; refactor

--- src/components/Unit/Presentation/MemberLists.tsx
+++ src/components/Unit/Presentation/MemberLists.tsx
@@ -9,23 +9,14 @@ import { MemberList } from "./MemberList";
 import { IUnitMember, UitsRole } from "../../types";
 
 export const MemberLists: React.SFC<IProps> = ({ members, title }) => {
-  const leaders: IUnitMember[] = [];
-  const related: IUnitMember[] = [];
-  const team: IUnitMember[] = [];
-  members && members.forEach(m => {
-    switch (m.role) {
-      case UitsRole.Leader:
-        leaders.push(m);
-        break;
-      case UitsRole.Related:
-        related.push(m);
-        break;
-      default:
-        team.push(m);
-        break;
-    }
-  })
-
+  // find all members with a matching role.
+  const inRole = (role: UitsRole) => 
+    (members || []).filter(m => m.role === role);
+    
+  const leaders = inRole(UitsRole.Leader);
+  const subLeads = inRole(UitsRole.Sublead);
+  const team = inRole(UitsRole.Member);
+  const related = inRole(UitsRole.Related);
 
   return <>
       {title && <h2 className="rvt-ts-32">{title}</h2>}
@@ -33,6 +24,7 @@ export const MemberLists: React.SFC<IProps> = ({ members, title }) => {
         <Col>
           {leaders.length > 0 && <MemberList title={"Leadership (" + leaders.length + ")"} members={leaders} showImages={true} />}
           {related.length > 0 && <MemberList title={"Related (" + related.length + ")"} members={related} />}
+          {subLeads.length > 0 && <MemberList title={"Subleadership (" + subLeads.length + ")"} members={subLeads} showImages={true} />}
           {team.length > 0 && <MemberList title={"Members (" + team.length + ")"} members={team} />}
         </Col>
       </Row>
