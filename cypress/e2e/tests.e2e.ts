f53d722bc0b1bb0014026f6cb8ef711d8cd6cee6
Venkata Bhupathi <venkatanbhupathi@gmail.com>
2021-08-23 10:28:51 -0400

    Revert "fixing viewports for ci tests"
    This reverts commit 799e5e9f0c172cd3543984d61ae357698f1be2df.

--- cypress/e2e/tests.e2e.ts
+++ cypress/e2e/tests.e2e.ts
@@ -19,7 +19,6 @@ const clearSessionStorage = (win: any) =>
     })
     
     it('adds unit member & tool -> removes them', () => {
-        cy.viewport(1280, 800)
         cy.visit('/', { onBeforeLoad: clearSessionStorage })
         cy.getAllByText(/log in/i).last().click()
         cy.visit('/units/2')
@@ -50,7 +49,6 @@ const clearSessionStorage = (win: any) =>
 describe('standard user', () => {
     
     it('cannot edit or delete a unit', () => {
-        cy.viewport(1280, 800)
         cy.visit('/', { onBeforeLoad: clearSessionStorage })
         cy.getAllByText(/log in/i).last().click()
         cy.visit('/units/1')
@@ -62,7 +60,6 @@ describe('standard user', () => {
 describe('unit leader', () => {
 
     it('can edit unit, members & tools', () => {
-        cy.viewport(1280, 800)
         cy.visit('/', { onBeforeLoad: clearSessionStorage })
         cy.getAllByText(/log in/i).last().click()
         cy.visit('/units/4')
