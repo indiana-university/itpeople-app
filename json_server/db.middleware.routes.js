e426891cb9dafc85ee76508cb2a9592db2b48fcd
Jason Francis <jkfranci@iu.edu>
2021-08-27 12:30:24 -0400

    Adding a big chunk of middleware to do the basics of what archiving does: It flips the bit on unit.active.

--- json_server/db.middleware.routes.js
+++ json_server/db.middleware.routes.js
@@ -1,15 +1,42 @@
+const { Container } = require("rivet-react");
+
 module.exports = (req, res, next) => {
+  const db = getDb();
+
   if (req.method == "DELETE") {
-    next();
-    res.status(204);
-    return res.end();
+    // Just say all deletes succed, except for unit archiving requests, which are special.
+    var skip = false;
+    if(req.path.startsWith("/units/") && req.path.endsWith("/archive")) {
+      const pathParts = req.path.split("/");
+      const unitId = pathParts[2];
+      
+      const unit = db.units.find(u => u.id == unitId);
+      if(!unit)
+      {
+        res.status(404);
+        return next();
+      }
+
+      // Flip the active bit
+      unit.active = !unit.active;
+      
+      // Write the changes to db.json
+      saveToDb("units", unit);
+
+      // Return the unit with a 200 OK
+      return res.send(unit);
+    }
+    if(skip == false)
+    {
+      next();
+      res.status(204);
+      return res.end();
+    }
   }
   // GET /people/**
   if (req.method != "GET") {
     return next();
   }
-
-  const db = getDb();
 
   if (req.path.startsWith("/people/")) {
     const pathParts = req.path.split("/");
@@ -43,4 +70,24 @@ function getDb() {
   var jsonPath = path.join(__dirname, "..", "src", "db.json");
   let rawdata = fs.readFileSync(jsonPath);
   return JSON.parse(rawdata);
+}
+
+function saveToDb(container, item) {
+  const fs = require("fs");
+  var path = require("path");
+  var jsonPath = path.join(__dirname, "..", "src", "db.json");
+  
+  var db = getDb();
+  // Find and update item.
+  for(let ix in db[container]) {
+    let dbItem = db[container][ix]
+    if(dbItem.id == item.id)
+    {
+      db[container][ix] = item;
+      break;
+    }
+  }
+
+  // Write changes to path.
+  fs.writeFileSync(jsonPath, JSON.stringify(db, null, 2));
 }
