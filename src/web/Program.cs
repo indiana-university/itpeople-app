7a59f9a35107efff9531c356d8d96107d61cdf2a
Jason Francis <jkfranci@iu.edu>
2021-10-14 16:36:43 -0400

    Actually got the injected Api client working, removing the helper class.

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -20,7 +20,29 @@ namespace web
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazoredSessionStorage();
-            
+            builder.Services.AddHttpClient("Api", client => {
+                // Get the BaseAddress from our configuration
+                client.BaseAddress = new Uri(Utils.Env(builder.Configuration, "API_URL", true));
+                
+                // Attempt to fetch the JWT from storage
+                // Get user data from the session
+                var sp = builder.Services.BuildServiceProvider();
+                var session = sp.GetService<ISyncSessionStorageService>();
+
+                var user = session.GetItem<AuthenticatedUser>("user");
+                var jwtString = user?.AccessToken ?? "";
+
+                // If we got a JWT use it to add an Authorization header for API requestes.
+                if(string.IsNullOrWhiteSpace(jwtString) == false)
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", $"bearer {jwtString}");
+                }
+                else
+                {
+                    throw new Exception("Could not find an access token in Session Storage, is the user currently logged in?");
+                }
+            });
+
             await builder.Build().RunAsync();
         }
     }
